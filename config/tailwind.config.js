1c7c68f426e184a204f9a923405ad816ea97932c
malonetheressa <theressadmalone@gmail.com>
2024-03-15 15:13:48 +0100

    :lipstick: reconfigure tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,35 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+    content: [
+        './public/*.html',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/views/**/*.{erb,haml,html,slim}',
+        './pages/**/*.{erb,html,js}',
+        './components/**/*.{erb,html,js}',
+        './../app/views/activity_logs/index.html.erb',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/container-queries'),
+    ],
+    purge: {
+        content: ['./../app/views/**/*.html.erb'],
+        safelist: [
+            'bg-blue-500',
+            'text-center',
+            'hover:opacity-100',
+            'lg:text-right',
+        ],
+    },
+};
